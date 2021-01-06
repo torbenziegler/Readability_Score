bf3e9749140bbeeeaf335b0c2e8b3c37438529e0
Ziegler Torben <tz023@hdm-stuttgart.de>
2021-01-06 12:06:44 +0100

    Added JavaDoc

---
+++ src/main/resources/JavaDoc/member-search-index.js
@@ -0,0 +1,26 @@
+memberSearchIndex = [{
+    "p": "ziegler.torben",
+    "c": "Processing",
+    "l": "calculateSyllables(String[])",
+    "u": "calculateSyllables(java.lang.String[])"
+}, {"p": "ziegler.torben", "c": "Calculation", "l": "Calculation()", "u": "%3Cinit%3E()"}, {
+    "p": "ziegler.torben",
+    "c": "Processing",
+    "l": "getPolysyllables()"
+}, {"p": "ziegler.torben", "c": "Processing", "l": "getSyllables()"}, {
+    "p": "ziegler.torben",
+    "c": "Main",
+    "l": "Main()",
+    "u": "%3Cinit%3E()"
+}, {"p": "ziegler.torben", "c": "Main", "l": "main(String[])", "u": "main(java.lang.String[])"}, {
+    "p": "ziegler.torben",
+    "c": "Processing",
+    "l": "Processing()",
+    "u": "%3Cinit%3E()"
+}, {
+    "p": "ziegler.torben",
+    "c": "Processing",
+    "l": "processText(String)",
+    "u": "processText(java.lang.String)"
+}, {"p": "ziegler.torben", "c": "Result", "l": "Result()", "u": "%3Cinit%3E()"}];
+updateSearchResults();
---
+++ src/main/resources/JavaDoc/type-search-index.js
@@ -0,0 +1,8 @@
+typeSearchIndex = [{"l": "All Classes", "u": "allclasses-index.html"}, {
+    "p": "ziegler.torben",
+    "l": "Calculation"
+}, {"p": "ziegler.torben", "l": "Main"}, {"p": "ziegler.torben", "l": "Processing"}, {
+    "p": "ziegler.torben",
+    "l": "Result"
+}];
+updateSearchResults();
